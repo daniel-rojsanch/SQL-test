b3a117d073023b9980e1cc10f810e55070776874
daniel-rojsanch <daniel.rojsanch@gmail.com>
2022-07-09 16:42:54 -0500

    :confused:

---
+++ practica1/practicando.sql
@@ -0,0 +1,18 @@
+## Crear una base de datos
+create database holamundo;
+
+## Que nos muestre las bases de datos que tenemos
+show databases;
+
+## seleccionar la bases de datos que queremos
+use holamundo;
+
+## Crear una tabla 
+create table alumno (
+id int,
+nombre varchar(250),
+edad int
+);
+
+
+
